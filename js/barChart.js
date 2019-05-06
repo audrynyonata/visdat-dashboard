c3633ecddcb2b3ac00edb8e6b214ca142ddf6a2d
audrynyonata <audrynyonata@gmail.com>
2019-05-06 21:22:10 +0700

    barchart pyramid

--- js/barChart.js
+++ js/barChart.js
@@ -1,56 +1,80 @@
 /* Source: http://blog.deontwikkelfabriek.nl/creating-a-population-pyramid-with-the-google-chart-tools/ */
+function drawBarChart(year) {
+   // grab the CSV
+   $.get("data/ekspor.csv", function(csvStringEkspor) {
+      $.get("data/impor.csv", function(csvStringImpor) {
+      // transform the CSV string into a 2-dimensional array
+      var arrayEkspor = $.csv.toArrays(csvStringEkspor, {onParseValue: $.csv.hooks.castToScalar});
+      var arrayImpor = $.csv.toArrays(csvStringImpor, {onParseValue: $.csv.hooks.castToScalar});
+      
+      // transform year header from number to text      
+      for(var i=1;i<=6;i++){
+        arrayEkspor[0][i] = arrayEkspor[0][i].toString()
+        arrayImpor[0][i] = arrayImpor[0][i].toString()
+      }
 
-function drawBarChart()
-{
+      // replace all ekspor with negative value (biar di kiri sumbu Y)
+      for(var j=1; j<arrayEkspor.length;j++){
+        for(var i=1;i<=6;i++){
+          arrayEkspor[j][i] = arrayEkspor[j][i] * -1
+        }
+      }
 
-  var dataArray = [
-    ['Age', 'Male', 'Female'],
-    ['0-4 years',   106, -104],
-    ['5-9 years',   91,  -86 ],
-    ['10-14 years', 79,  -77 ],
-    ['15-19 years', 68,  -64 ],
-    ['20-24 years', 62,  -58 ],
-    ['25-29 years', 56,  -53 ],
-    ['30-34 years', 51,  -46 ],
-    ['35-39 years', 48,  -41 ],
-    ['40-44 years', 43,  -35 ],
-    ['45-49 years', 39,  -30 ],
-    ['50-54 years', 33,  -27 ],
-    ['55-59 years', 32,  -25 ],
-    ['60-64 years', 27,  -20 ],
-    ['64-69 years', 19,  -16 ],
-    ['70-74 years', 13,  -12 ],
-    ['75-79 years', 8,   -7  ],
-    ['80-84 years', 3,   -3  ],
-    ['85-89 years', 1,   -1  ],
-    ['90-94 years', 0,   0   ],
-    ['95+ years',   0,   0   ]
-  ];
+      var yr = 1;
+      switch (year) {
+        case 2014: yr = 2; break;
+        case 2015: yr = 3; break;
+        case 2016: yr = 4; break;
+        case 2017: yr = 5; break;
+        case 2018: yr = 6; break;
+        default: yr = 1; break;
+      }
 
-  var data = google.visualization.arrayToDataTable(dataArray);
+      var arrayFinal = [];
+      arrayFinal[0] = ['Sektor', 'Ekspor', 'Impor']
+      for (var i=1;i<arrayEkspor.length;i++) {
+        arrayFinal[i] = []
+        arrayFinal[i].push(arrayEkspor[i][0],arrayEkspor[i][yr],arrayImpor[i][yr])
+      }
 
-  var options = {
-    isStacked: true,
-    hAxis: {
-      format: ';'
-    },
-    vAxis: {
-      direction: -1
-    },
-    height: 500,
-    animation: {
-      duration: 2000,
-      easing: 'out'
-    }
-  };
+      // this new DataTable object holds all the data
+      var data = new google.visualization.arrayToDataTable(arrayFinal);
 
+      data.sort([{column: 1}])
+      // this view can select a subset of the data at a time
+      var view = new google.visualization.DataView(data);
 
-  var formatter = new google.visualization.NumberFormat({
-    pattern: ';'
+      //view.setColumns([0,1,2]);
+
+      // set chart options
+      var options = {
+        isStacked: true,
+        hAxis: {
+          format: ',###.#;,###.#'
+        },
+        height: 500,
+        animation: {
+          duration: 2000,
+          easing: 'out'
+        },
+        tooltip: {
+          isHtml: true,
+          trigger: 'both'
+        },
+        colors: ['#8BC34A', '#F44336']
+      };
+
+      var formatter = new google.visualization.NumberFormat({
+        pattern: '.#;,.#',
+        suffix: ' Juta US$'
+      });
+
+      formatter.format(data, 1)
+      formatter.format(data, 2)
+
+      // create the chart object and draw it
+      var chart = new google.visualization.BarChart(document.getElementById('view3'));
+      chart.draw(view, options);
+    });
   });
-
-  formatter.format(data, 2)
-
-  var chart = new google.visualization.BarChart(document.getElementById('view3'));
-  chart.draw(data, options);
 }
